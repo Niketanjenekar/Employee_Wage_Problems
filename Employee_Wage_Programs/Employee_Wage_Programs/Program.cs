296a6506acec848a2eebf2dbaadaf54f8243ea9c
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-06-21 14:17:33 +0530

    [Niketan]Add:Welcome to Employee Wage Program

---
+++ Employee_Wage_Programs/Employee_Wage_Programs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Wage_Programs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("**** Welcome to Employee Wage Computation Program ****");
+
+            Console.ReadLine();
+        }
+    }
+}
